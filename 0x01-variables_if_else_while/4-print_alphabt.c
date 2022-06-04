344d78ad1d3f28dc75675dfa668a5a6a1733d9a1
Obinna <bekwes2005@yahoo.com>
2022-06-03 21:28:45 -0700

    Task_4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,45 @@
+
+
+#include <stdio.h>
+
+
+
+/**
+ *
+ * *  Write a program that prints the alphabet in lowercase, followed by a new line.
+ *
+ *  */
+
+
+
+char main(void)
+
+{
+
+	char ch;
+
+	for (ch = 97; ch <= 122; ch++)
+
+	{
+
+		if (ch != 'q' && ch != 'e')
+
+		{
+
+
+
+				putchar(ch);
+
+		}
+
+	}
+
+
+
+	putchar(10); /* this is an ascii code for new line */
+
+
+
+	return (0);
+
+}
