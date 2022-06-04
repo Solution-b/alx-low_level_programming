ac1ccfb8a3a00b6388420317d51c4cfa9d301410
Obinna <bekwes2005@yahoo.com>
2022-06-03 20:50:40 -0700

    Task_2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,35 @@
+
+
+#include <stdio.h>
+
+
+
+/**
+ *
+ * *  a program that prints the alphabet in lowercase, followed by a new line.
+ *
+ *  */
+
+
+
+int main(void)
+
+{
+
+	int ch;
+
+	for (ch = 97; ch <= 122; ch++)
+
+	{
+
+			putchar(ch);
+
+	}
+
+	putchar(10);
+
+
+
+	return (0);
+
+}
