880d81bb8fb00721aff45dfe30098750209c77ae
Solution-b <bekwes2005@yahoo.com>
2022-06-07 01:32:25 +0000

    task6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+
+#include <stdio.h>
+
+/**
+ * *  * main - Entry point
+ * *  *  *
+ * *   *Return : always 0 (success)
+ * *  **/
+
+int main(void)
+{	int a;
+	long int b;
+	long long int C;
+	char d;
+	float e;
+		
+	printf("Size of an char: %lu byte(s)\n", (unasigned long) sizeoff(d));
+	printf("Size of an int: %lu byte(s)\n", (unasigned long) sizeof(a));
+	printf("Size of a long int: %lu byte(s)\n", (unasigned long) sizeoff(b));
+	printf("Size of an long long int: %lu byte(s)\n", (unasigned long) sizeof(c));
+	printf("Size of a float: %lu byte(s)\n", (unasigned long) sizeoff(e));
+
+	return (0);
+}
