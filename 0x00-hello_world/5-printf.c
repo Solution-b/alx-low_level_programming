79fd11a7cd8cfd14b0fd51eff2d4ec588200f804
Solution-b <bekwes2005@yahoo.com>
2022-06-07 01:12:37 +0000

    task4

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,9 +6,9 @@
 	 * *   *Return : always 0 (success)
 	 * *  **/
 
-	int main(void)  #trying different things
+	int main(void) 
 
 		put("\" With proper grammar, but the outcome is a piece of art")
 
-			return (0);  #returs
+			return (0); 
 }
