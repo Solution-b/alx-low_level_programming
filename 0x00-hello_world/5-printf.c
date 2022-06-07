e6ad9699c4e33f40002a9a85c9213f825fd30c10
Solution-b <bekwes2005@yahoo.com>
2022-06-07 01:18:48 +0000

    task4

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-{
+
+
 	/**
 	 * *  * main - Entry point
 	 * *  *  *
@@ -7,7 +8,7 @@
 	 * *  **/
 
 	int main(void) 
-
+{
 		put("\" With proper grammar, but the outcome is a piece of art")
 
 			return (0); 
