3a3f58d60c0935a6897b935c4442b781585049f8
Solution-b <bekwes2005@yahoo.com>
2022-06-07 01:41:05 +0000

    task6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,3 @@
-
-#include <stdio.h>
-
 /**
  * *  * main - Entry point
  * *  *  *
@@ -8,17 +5,18 @@
  * *  **/
 
 int main(void)
-{	int a;
+{
+	int a;
 	long int b;
 	long long int C;
 	char d;
-	float e;
+	float f;
 		
-	printf("Size of an char: %lu byte(s)\n", (unasigned long) sizeoff(d));
-	printf("Size of an int: %lu byte(s)\n", (unasigned long) sizeof(a));
-	printf("Size of a long int: %lu byte(s)\n", (unasigned long) sizeoff(b));
-	printf("Size of an long long int: %lu byte(s)\n", (unasigned long) sizeof(c));
-	printf("Size of a float: %lu byte(s)\n", (unasigned long) sizeoff(e));
+	printf("Size of an char: %lu byte(s)\n", (unasigned long)sizeof(d));
+	printf("Size of an int: %lu byte(s)\n", (unasigned long)sizeof(a));
+	printf("Size of a long int: %lu byte(s)\n", (unasigned long)sizeoff(b));
+	printf("Size of an long long int: %lu byte(s)\n", (unasigned long)sizeof(c));
+	printf("Size of a float: %lu byte(s)\n", (unasigned long) sizeoff(f));
 
 	return (0);
 }
