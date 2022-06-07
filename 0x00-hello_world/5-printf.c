be5f2d8883021ec352253de8b600a55116b0c045
Solution-b <bekwes2005@yahoo.com>
2022-06-07 01:09:38 +0000

    task4

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+{
+	/**
+	 * *  * main - Entry point
+	 * *  *  *
+	 * *   *Return : always 0 (success)
+	 * *  **/
+
+	int main(void)  #trying different things
+
+		put("\" With proper grammar, but the outcome is a piece of art")
+
+			return (0);  #returs
+}
