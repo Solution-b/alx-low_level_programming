cdde1a16204ab463ca8b0213ccaf4a7214d52f2d
Solution-b <72709066+Solution-b@users.noreply.github.com>
2022-06-08 05:03:43 +0100

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
-/** main - prints putchar
- * * Return: always 0
- * */
+/**
+ *  main - prints putchar
+ * Return: always 0
+ */
 int main(void)
 {
 	_putchar('_');
@@ -16,5 +17,4 @@ int main(void)
 	_putchar('\n');
 
 	return (0);
-	
 }
