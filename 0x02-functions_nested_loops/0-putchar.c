ee29d8568edb554702d3a6399f477c0a48589583
Solution-b <72709066+Solution-b@users.noreply.github.com>
2022-06-08 04:11:54 +0100

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,9 @@
-#include "main.h";
+#include "main.h"
 
 /** main - prints putchar
 * Return: always 0
 */
+int main(void)
 {
 	_putchar('_');
 	_putchar('p');
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,1 +1,2 @@
-Test
+I am tired but i will keep trying.
+This is for the funtion of the work
