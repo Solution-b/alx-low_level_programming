c2a8ab2e9086a9435e42bcac7263af18b09382d7
Solution-b <72709066+Solution-b@users.noreply.github.com>
2022-06-09 02:59:21 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,9 @@
 #include "main.h"
-<<<<<<< HEAD
 
 /**
- *  main - prints putchar
- * Return: always 0
- */
-=======
-/** main - prints putchar
+* main - prints putchar
 * Return: always 0
 */
->>>>>>> e7e8b035fe57bb4e607e660b70f742551e8abc9c
 int main(void)
 {
 	_putchar('_');
@@ -22,11 +16,5 @@ int main(void)
 	_putchar('r');
 	_putchar('\n');
 
-<<<<<<< HEAD
 	return (0);
-
 }
-=======
-	Return(0);
-}
->>>>>>> e7e8b035fe57bb4e607e660b70f742551e8abc9c
