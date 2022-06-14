103e14acfd360ce535a60afe8cd1df44649849bc
Obinna <bekwes2005@yahoo.com>
2022-06-13 20:54:02 -0700

    6. Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts2 -> function that prints every other character of a string,
+ * starting with the first character
+ * followed by a new line.
+ * @str: Str parameter
+ */
+void puts2(char *str)
+{
+	int len, i;
+
+	len = strlen(str);
+	for (i = 0; i < len; i += 2)
+		_putchar(str[i];
+	_putchar ('\n')
+}
