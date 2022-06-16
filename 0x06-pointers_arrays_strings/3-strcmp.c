10e8a52df5621ab236d00fcf2a0be7cf360914a0
Obinna <bekwes2005@yahoo.com>
2022-06-15 17:54:26 -0700

    2. strncpy

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,9 @@
+#include "Main.h"
+#include <string.h>
+/**
+* _strncpy - a function that copies a string
+* @s1: Parameter 1
+* @s2: Paragram 2
+* Return: integer
+*/
+int char *_strncpy(char *dest, char *src, int n);
