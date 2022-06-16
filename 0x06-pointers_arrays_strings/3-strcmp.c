de6109f2aa97f8bcbbd2bb0b8db1fd472c9b946c
Obinna <bekwes2005@yahoo.com>
2022-06-15 18:31:56 -0700

    3. strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -19,13 +19,12 @@ int _strcmp(char *s1, char *s2);
   /* iterate through src and compare it with dest */
 	while (s1[i] != '\0' && s2[i] != '\0')
 	{
- /**
- * if they differ by a single character don't iterate further
-*/
+ /** if they differ by a single character don't iterate further */
 		if (s1[i] != s2[i])
 	{
 /** Replaces  the difference between the two characters*/
 		r = s1[i] - s2[i];
+		break;
 	}
 		else
 		{
