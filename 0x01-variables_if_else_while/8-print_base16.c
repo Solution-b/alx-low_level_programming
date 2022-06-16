4feb66585dc6049853513aa4b8a3a7414a760783
Solution-b <72709066+Solution-b@users.noreply.github.com>
2022-06-16 22:24:15 +0100

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,47 +1,27 @@
-
-
 #include <stdio.h>
 
-
+/**
+ *main -> assign a random number to the variable n each time it is executed
+ *and print the last digit of the number stored in the variable n
+ *Return: Always 0 (Success)
+ */
 int main(void)
+{
+char ch;
+int n;
 
+for (n = 48; n <= 57; n++)
 {
 
-\*
-* headecimal 0 1 2 3 4 5  6 7 8 9 a b c d e 
-*
-*\
+putchar(n);
+}
+for (ch = 'a'; ch <= 'f'; ch++)
+{
 
-		char ch;
-
-	int n;
-
-	for (n = 48; n <= 57; n++)
-
-	{
-
-			putchar(n);
-
-	}
-
-	for (ch = 'a'; ch <= 'f'; ch++)
-
-	{
-
-			putchar(ch);
-
-	}
-
-	putchar(10); \*this is ancii code for new line*\
-
-
-
-		return (0);
-
+putchar(ch);
 }
 
+putchar(10);
 
-
-
-
-
+return (0);
+}
