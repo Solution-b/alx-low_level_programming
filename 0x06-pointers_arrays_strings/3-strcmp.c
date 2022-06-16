a82094b3cf116c8a74b9b17966a92d9316748aba
Obinna <bekwes2005@yahoo.com>
2022-06-15 19:37:21 -0700

    4. I am a kind of paranoid in reverse

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,9 +2,9 @@
 #include <string.h>
 /**
 * _strcmp - a function that compares two strings
-* @s1: string 1 input to compare
-* @s2: against this other string 2
-* Return: integer 0 if s1 and s2 are equal
+* @s1: string one input to compare
+* @s2: against this other string two
+* Return: integer zero if s1 and s2 are equal
 * negative integer if the stopping character
 * in @s1 was less than the stopping
 * character in @s2positive integer if the stopping character
