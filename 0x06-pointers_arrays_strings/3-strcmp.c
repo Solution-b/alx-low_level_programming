04663e80b3c815ea319d3513120e76731f606f15
Obinna <bekwes2005@yahoo.com>
2022-06-15 19:17:44 -0700

    3. strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -13,19 +13,8 @@
 */
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0, r;
+	int result;
 
-	while (s1[i] != '\0' && s2[i] != '\0')
-	{
-		if (s1[i] != s2[i])
-		{
-			r = s1[i] - s2[i];
-			break;
-		}
-		else
-			r = s1[i] - s2[i];
-	}
-	i++;
-}
-return (r);
+	result = strcmp(s1, s2);
+	return (result);
 }
