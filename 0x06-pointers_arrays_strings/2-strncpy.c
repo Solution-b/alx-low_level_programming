8673b8da37c26fc1f0984c17ee142b53379b8a7d
Obinna <bekwes2005@yahoo.com>
2022-06-15 19:12:13 -0700

    2. strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -7,8 +7,8 @@
  * @n: parameter3
  * Return: string
  */
-char *_strncpy(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n)
 {
-	strncpy(dest,src,n);
+	strncpy(dest, src, n);
 	return (dest);
 }
