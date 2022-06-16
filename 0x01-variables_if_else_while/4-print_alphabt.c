d14fb0dd770ad86154faed08bb446289398e2727
Solution-b <72709066+Solution-b@users.noreply.github.com>
2022-06-16 22:17:40 +0100

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,45 +1,22 @@
-
-
 #include <stdio.h>
 
-
-
 /**
- *
- * *  Write a program that prints the alphabet in lowercase, followed by a new line.
- *
- *  */
-
-
-
+ *main -> assign a random number to the variable n each time it is executed
+ *and print the last digit of the number stored in the variable n
+ *Return: Always 0 (Success)
+ */
 int main(void)
-
 {
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+if (ch != 'q' && ch != 'e')
+{
+putchar(ch);
+}
+}
 
-	char ch;
+putchar(10); /* this is an ascii code for new line*/
 
-	for (ch = 97; ch <= 122; ch++)
-
-	{
-
-		if (ch != 'q' && ch != 'e')
-
-		{
-
-
-
-				putchar(ch);
-
-		}
-
-	}
-
-
-
-	putchar(10); /* this is an ascii code for new line */
-
-
-
-	return (0);
-
+return (0);
 }
