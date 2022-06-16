80f80dbb4850a94851fdb413511da5ae002635c8
Solution-b <72709066+Solution-b@users.noreply.github.com>
2022-06-16 22:22:46 +0100

    Update 7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,33 +1,21 @@
-
-
 #include <stdio.h>
 
-
-
-/*  Write a program that prints the lowercase alphabet in reverse, followed by a new line.*/
-
-
-
+/**
+ *main -> assign a random number to the variable n each time it is executed
+ *and print the last digit of the number stored in the variable n
+ *Return: Always 0 (Success)
+ */
 int main(void)
+{
+char ch;
 
+for (ch = 'z'; ch >= 'a'; ch--)
 {
 
-	char ch;
-
-	for (ch = 'z'; ch >= 'a'; ch++)
-
-	{
-
-			putchar(ch);
-
-	}
-
-	putchar(10); \*this is ancii code for new line*\
-
-
-
-		return (0);
-
+putchar(ch);
 }
 
+putchar(10); /* this is an ascii code for new line*/
 
+return (0);
+}
