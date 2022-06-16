9f8238aa1eaf8137375431511d68c71fb834ea00
Solution-b <72709066+Solution-b@users.noreply.github.com>
2022-06-16 22:41:25 +0100

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
 
-
-	/**
-	 * *  * main - Entry point
-	 * *  *  *
-	 * *   *Return : always 0 (success)
-	 * *  **/
-
-	int main(void) 
+/**
+* main - prints the size of various types
+* Return: 0 if exited properly, non-zero otherwise
+*/
+int main(void)
 {
-		put("\" With proper grammar, but the outcome is a piece of art")
-
-			return (0); 
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
